1a9fba6d4ab3c819812519744c2e70bf09c3429f
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-03-10 19:12:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (20.75%)

---
+++ 0070-climbing-stairs/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    private:
+    int f(int ind,vector<int> &dp){
+        if(ind==0) return 1;
+        if(ind==1) return 1;
+        if(dp[ind]!=-1) return dp[ind];
+       int left=f(ind-1,dp);
+        int right=f(ind-2,dp);
+        return dp[ind]=left+right;
+    }
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
+    }
+};
