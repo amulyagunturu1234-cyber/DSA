decac15ad8b55dd1986e60b92c3e2907c70a5c55
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-03-23 03:30:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.4 MB (63.12%)

---
+++ 0213-house-robber-ii/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    private:
+    int f(vector<int>& nums){
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=0;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=prev2;
+            int notTake=0+prev;
+            int curr=max(take,notTake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>temp1,temp2;
+        for(int i=0;i<n;i++){
+            if(n==1) return nums[i];
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=n-1)temp2.push_back(nums[i]);
+        }
+    return max(f(temp1),f(temp2));
+    }
+};
