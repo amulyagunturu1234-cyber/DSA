e93af8e8cff6c857d8e7459cf3a97a05ababdab7
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-04-05 13:50:11 +0000

    [LeetCode Sync] Runtime - 19 ms (5.21%), Memory - 28.1 MB (6.54%)

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int f(int ind,int buy,vector<int>& prices,vector<vector<int>>& dp){
+        int n=prices.size();
+        if(ind==n) return 0;
+        int profit;
+        if(dp[ind][buy]!=-1)return dp[ind][buy];
+        if(buy){
+           dp[ind][buy]=profit=max((-prices[ind]+f(ind+1,0,prices,dp)),(0+f(ind+1,1,prices,dp)));
+        }
+        else dp[ind][buy]=profit=max((prices[ind]+f(ind+1,1,prices,dp)),(0+f(ind+1,0,prices,dp)));
+        return dp[ind][buy]=profit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return f(0,1,prices,dp);
+    }
+};
