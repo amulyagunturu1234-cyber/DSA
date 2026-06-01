0463dab91d636eead957f511c78dfd5bf77e8233
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-06-01 05:45:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.3 MB (52.67%)

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+        int total = 0;
+        for (int i = 0; i < cost.size(); i++) {
+            if ((i + 1) % 3 == 0) continue; // every 3rd candy is free
+            total += cost[i];
+        }
+        return total;
+    }
+};
