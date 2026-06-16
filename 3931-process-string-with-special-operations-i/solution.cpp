7075832337e88f02c7ce2ed0d32e0644d9604cea
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-06-16 07:44:10 +0000

    [LeetCode Sync] Runtime - 3 ms (74.89%), Memory - 39.9 MB (43.73%)

---
+++ 3931-process-string-with-special-operations-i/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result = "";
+        
+        for (char c : s) {
+            if (c == '*') {
+                if (!result.empty()) {
+                    result.pop_back();
+                }
+            } else if (c == '#') {
+                result = result + result;
+            } else if (c == '%') {
+                reverse(result.begin(), result.end());
+            } else {
+                result += c;
+            }
+        }
+        
+        return result;
+    }
+};
