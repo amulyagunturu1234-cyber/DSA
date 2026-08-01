5c479c92b1845eaf41559825c0fe6e3c35992cc3
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-08-01 04:09:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.1 MB (32.64%)

---
+++ 0486-predict-the-winner/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    private:
+    int solve(int i,int j,vector<int>&nums,vector<vector<int>> &dp){
+        if(i==j) return nums[i];
+        if(dp[i][j]!=INT_MIN) return dp[i][j];//overlapping subacses
+        int tkleft=nums[i]-solve(i+1,j,nums,dp);
+        int tkright=nums[j]-solve(i,j-1,nums,dp);//instead of storing the both palyers sum seperately we are storing the difference a dry run helps
+        return dp[i][j]=max(tkleft,tkright);
+    }
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
+        return solve(0,n-1,nums,dp)>=0;
+    }
+};
