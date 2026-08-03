30ef008c275e2bacb1b87f10cf045e52eda7c1e8
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-08-03 18:37:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 31.2 MB (74.64%)

---
+++ 0222-count-complete-tree-nodes/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    int findLeftHeight(TreeNode* node){
+        int ht=0;
+        while(node){
+            ht++;
+            node=node->left;
+        }
+        return ht;
+    }
+    int findRightHeight(TreeNode* node){
+        int ht=0;
+        while(node){
+            ht++;
+            node=node->right;
+        }
+        return ht;
+    }
+    public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        int lh=findLeftHeight(root);
+        int rh=findRightHeight(root);
+        if(lh==rh) return(1<<lh)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
+    }
+};
