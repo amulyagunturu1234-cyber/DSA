fe0031e0b06c273b68bd72eaca40a7c0c13496cb
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-08-16 23:41:39 +0530

    Time: 10 ms (19.80%) | Memory: 131.3 MB (69.29%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+
+        // Count stones based on remainder modulo 3
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        // If number of 0-remainder stones is even
+        if (cnt[0] % 2 == 0) {
+            // Alice needs both 1 and 2 remainder stones
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
+
+        // If number of 0-remainder stones is odd
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
