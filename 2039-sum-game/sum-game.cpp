d9175bb35a01ba8e73a8d367ba012addb4122079
Amulya Gunturu <amulyagunturu1234@gmail.com>
2026-08-23 23:37:47 +0530

    Time: 7 ms (21.45%) | Memory: 14 MB (67.83%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?')
+                leftQ++;
+            else
+                leftSum += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?')
+                rightQ++;
+            else
+                rightSum += num[i] - '0';
+        }
+
+        int q = rightQ - leftQ;
+        int diff = leftSum - rightSum;
+
+        // Odd number of '?' -> Alice wins
+        if ((leftQ + rightQ) % 2 == 1)
+            return true;
+
+        // Bob wins only if the initial difference can be
+        // exactly compensated by the remaining '?'s.
+        return 2 * diff != 9 * q;
+    }
+};
